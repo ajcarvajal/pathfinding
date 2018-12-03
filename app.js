8df0d43276f6842e09e60d9e4cc3da949151b403
Aj Carvajal <alanjcarv@gmail.com>
2018-12-02 20:05:40 -0900

    robot follows path

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //a* implementation uses code and ideas from:
 //https://briangrinstead.com/blog/astar-search-algorithm-in-javascript/
 
+let t0;
+let t1;
+
 let width = Math.min(window.innerWidth,window.innerHeight) - 20;
 let height = width;
 
@@ -35,7 +38,6 @@ function Node(x,y) {
     this.display = function() {
         fill(this.color);
         stroke(50);
-        //strokeWeight(2);
         rect(this.xpos * width / cols, this.ypos * height / rows, (width / cols) - 1, (height / rows) - 1);
     }
 }
@@ -43,23 +45,46 @@ function Node(x,y) {
 function Robot(x,y) {
     this.xpos = x;
     this.ypos = y;
+    this.path;
+    this.nextNode = null;
 
-    this.move = function(newX, newY) {
-        this.xpos = lerp(this.xpos, newX, 0.0001);
-        this.ypos = lerp(this.ypos, newY, 0.0001);
-        this.display();
+    this.move = function() {
+        var moved = false;
+        if(!solved) {
+            resetNodes();
+            init();
+        
+        }else if(this.path != null) {
+            if(this.nextNode == null) this.nextNode = this.path.pop();
+            if(this.xpos < this.nextNode.xpos - 0.01 || this.xpos > this.nextNode.xpos + 0.01) {
+                //this.xpos = lerp(this.xpos, this.nextNode.xpos, 0.01);
+                this.xpos +=  (this.nextNode.xpos - this.xpos) / 2;
+                moved = true;
+            }
+            if(this.ypos < this.nextNode.ypos - 0.01 || this.ypos > this.nextNode.ypos + 0.01) {
+                //this.ypos = lerp(this.ypos, this.nextNode.ypos, 0.01);
+                this.ypos += (this.nextNode.ypos - this.ypos) / 2;
+                moved = true;
+            }
+            grid[Math.round(this.xpos)][Math.round(this.ypos)].setColor(150,0,0);
+            if(!moved && this.path.length > 0 ) {
+                this.nextNode = this.path.pop();
+            }
+            this.show();
+        }
     }
-    this.display = function() {
+    this.show = function() {
         fill(150,0,0);
-        stroke(0);
-        strokeWeight(3);
-        rect(this.xpos * width / cols, this.ypos * height / rows, (width / cols) - 1, (height / rows) - 1);
+        noStroke();
+        rect(this.xpos * width / cols, this.ypos * height / rows, (width / cols)-1, (height / rows)-1, 20);
+
     }
 }
 
+
 //setup grid
-cols = 30;
-rows = 30;
+cols = 100;
+rows = 100;
 grid = new Array(cols);
 
 for(var i = 0; i < cols; ++i) {
@@ -74,13 +99,22 @@ for(var i = 0; i < cols; ++i) {
 
 //setup gui
 let ControlMenu = function() {
-    this.Diagonals = true;
+    this.Diagonals = false;
+    this.Reset = function() {  
+        for(var i = 0; i < cols; ++i) {
+            for(var j = 0; j < rows; ++j) {
+                grid[i][j] = new Node(i,j);
+            }
+        }
+        init();
+    }
 }
 
 let ControlPanel = new ControlMenu();
 let gui = new dat.GUI();
 gui.width = window.innerWidth / 4;
 gui.add(ControlPanel, 'Diagonals');
+gui.add(ControlPanel, 'Reset');
 
 
 
@@ -89,6 +123,9 @@ gui.add(ControlPanel, 'Diagonals');
 //////////////////////////////
 
 function setup() {
+    frameRate(60);
+    t0 = millis();
+    t1 = 0;
     window.addEventListener('mousemove', onMouseMove, false);
     window.addEventListener('mousedown', onMouseDown, false);
     window.addEventListener('mouseup', onMouseUp, false);
@@ -96,6 +133,7 @@ function setup() {
     createCanvas(width,height);
     
     init();
+    displayGrid();
 }
 
 function init() {
@@ -106,15 +144,15 @@ function init() {
     end = grid[cols-1][rows-1];
     end.setColor(0,150,0);
     current = start;
+    robot = new Robot(0, 0);
+    
 
     openSet.push(start);
 
-    robot = new Robot(start.xpos,start.ypos);
 }
-
 function resetNodes() {
-    for(var i = 0; i < cols; ++i) {
-        for(var j = 0; j < rows; ++j) {
+    for(var i = Math.round(robot.xpos); i < cols; ++i) {
+        for(var j = Math.round(robot.ypos); j < rows; ++j) {
             if(!grid[i][j].obstacle) grid[i][j] = new Node(i,j);
         }
     }
@@ -132,15 +170,53 @@ function onMouseMove(event) {
 
 function onMouseDown() {
     mouse_down = true;
-    resetNodes();
-    init();
-
     if(mouse_position.x < cols && mouse_position.y < rows) {
+        resetNodes();
+        openSet = new BinaryHeap(function(node) {return node.f});
+        solved = false;
+    
+        start = grid[Math.round(robot.xpos)][Math.round(robot.ypos)];
+        end = grid[cols-1][rows-1];
+        end.setColor(0,150,0);
+        current = start;
+    
+        openSet.push(start);
+
+        let x = mouse_position.x;
+        let y= mouse_position.y;
+
+        if(grid[x-1] && grid[x-1][y]) {
+            grid[x-1][y].obstacle = true;
+            grid[x-1][y].setColor(129,59,9);
+            grid[x-1][y].display();
+        }
+    
+        // East
+        if(grid[x+1] && grid[x+1][y]) {
+            grid[x+1][y].obstacle = true;
+            grid[x+1][y].setColor(129,59,9);
+            grid[x+1][y].display();
+        }
+    
+        // South
+        if(grid[x] && grid[x][y-1]) {
+            grid[x][y-1].obstacle = true;
+            grid[x][y-1].setColor(119,49,0);
+            grid[x][y-1].display();
+        }
+    
+        // North
+        if(grid[x] && grid[x][y+1]) {
+            grid[x][y+1].obstacle = true;
+            grid[x][y+1].setColor(75,50,50);
+            grid[x][y+1].display();
+        }
         let target = grid[mouse_position.x][mouse_position.y];
         target.obstacle = true;
-        target.setColor(139,69,19);
+        target.setColor(129,59,9);
         target.display();
     }
+    t0 = millis();
 
 
 }
@@ -156,10 +232,16 @@ function onMouseUp() {
 
 function draw() {
     requestAnimationFrame(draw);
+    t0 = millis();
+
     if(!solved && openSet.size() > 0){
         updateSets();
+        
     }
-
+    else if (!mouse_down && t0 - t1 > 10){
+        t1 = millis();
+        robot.move();
+    }
 }
 
 function updateSets() {
@@ -201,13 +283,15 @@ function updateSets() {
     }
 }
 
-function drawPath(current) {
+function drawPath(node) {
+    var current = node;
+    robot.path = [];
     while (current.parent) {
       current = current.parent;
-      grid[current.xpos][current.ypos].setColor(75,75,255);   
+      robot.path.push(current);
+      grid[current.xpos][current.ypos].setColor(75,75,255);
     }
   }
-
 
 function getNeighbors(grid, current) {
     let ret = [];
